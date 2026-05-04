2c20eb80c126e49af24fcad5833b231b378fee7d
carmenvij1964-cyber <carmenvij1964@gmail.com>
2026-05-03 23:40:15 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Aquí es donde Sugo o MacroDroid enviarán la información
 @app.route('/webhook', methods=['POST'])
